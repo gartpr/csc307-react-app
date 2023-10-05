303513dbc0ace6b823e623c731ac9f89bf86dcb7
gartpr <4jetty@comcast.net>
2023-10-05 10:53:50 -0700

    POST working

--- packages/express_backend/backend.js
+++ packages/express_backend/backend.js
@@ -40,7 +40,7 @@ const users = {
           id: 'zap555', 
           name: 'Dennis',
           job: 'Bartender',
-       }
+       },
     ]
  }
 
@@ -56,7 +56,32 @@ const users = {
     }
 });
 
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+    
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id); 
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+});
+
 const findUserByName = (name) => { 
     return users['users_list']
         .filter( (user) => user['name'] === name); 
 }
+
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
